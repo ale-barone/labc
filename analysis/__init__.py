ccdffce341cf6fb47de69a3b2bfecb911d7b36b1
Alessandro Barone <ab1n19@soton.ac.uk>
2022-08-07 00:39:04 +0100

    Draft new structure

--- analysis/__init__.py
+++ analysis/__init__.py
@@ -8,8 +8,8 @@
 # - (choosing the file format we are going to work with? )
 
 import numpy as np
-from LatticeABC import dataManager as _dM
-from LatticeABC.dataManager import dataContainer as _dC
+from labc import data as _dM
+from labc.data import container as _dC
 import os
 from os.path import dirname, abspath 
 
@@ -33,38 +33,28 @@ def _print_title(title: str) -> None:
 # - if not, it recomputes the quantity on the fly and store the results it into a cache
 #   in case it needs to be recomputed somewhere else in the same run
 # - (if told so) write actual data from the cache
-
-# I could implement this nicely
-class database:
-    pass
-
-class database_file:
-    pass
-
-class database_eval:
-    pass
-
-class database_func:
-    pass
-
-class database_obj:
-    pass
-
-class databasetmp:
-    
-    def __init__(self, analysis):
-        self.analysis = analysis
-
-
-    def register(self, an_func):
-        self.an_func = an_func
     
     
+class Globals:
+    # so far stupid wrapper of a dictionary... maybe we can make a smart use
+    # of this
+
+    def __init__(self):
+        self._register = {}
+
+    def add_global(self, key, value):
+        self._register[key] = value 
+    
+    def dump(self):
+        print(self._register)
+
+    def get(self, key):
+        return self._register[key]
 
         
 
 
-class analysis:
+class Analysis:
     """Initializer for the analysis."""
 
     def __init__(self, statsType, tsrc_list, config_list):
@@ -73,34 +63,36 @@ class analysis:
         self.statsType = statsType
         
         # dictionary for global variables of the analysis
-        self.globals = {}
         
-        # database for files (saved data)
-        self.database_file = database_file()
-        self.files = database_file()
+        # # database for files (saved data)
+        # self.database_file = database_file()
+        # self.files = database_file()
 
-        # database for useful function in the analysis
-        # i.e. fits
-        self.database_eval = database_eval()
-        self.eval = database_eval()
+        # # database for useful function in the analysis
+        # # i.e. fits
+        # self.database_eval = database_eval()
+        # self.eval = database_eval()
         
-        # database that combines files and useful functions
-        # (and allow me to switch from the 2 with self.onthefly)
-        self.database = database()
-        self.get = database()
-        # TODO: think about this a bit better
-        self.onthefly = False
+        # # database that combines files and useful functions
+        # # (and allow me to switch from the 2 with self.onthefly)
+        # self.database = database()
+        # self.get = database()
+        # # TODO: think about this a bit better
+        # self.onthefly = False
         
-        # database for object that depends on the analysis object
-        self.objdatabase = database()
-        self.obj = database()
+        # # database for object that depends on the analysis object
+        # self.objdatabase = database()
+        # self.obj = database()
     
-    def copy(self):
-        new = type(self)(self.statsType, self.tsrc_list, self.config_list)
-        new.globals = self.globals
-        new.database = self.database
-        new._update_database()
-        return new
+    def set_globals(self, globs):
+        self.globs = globs
+    
+    # def copy(self):
+    #     new = type(self)(self.statsType, self.tsrc_list, self.config_list)
+    #     new.globals = self.globals
+    #     new.database = self.database
+    #     new._update_database()
+    #     return new
 
     def print_setup(self):
         _print_title("ANALYSIS SETUP")
@@ -109,100 +101,99 @@ class analysis:
         print("# statsType   = ", 'to be implemented')
         _print_bar()
     
-    def dataStats(self, file: str, tsrc_list: list=None) -> _dM.dataStats:
-        tsrc_list = self.tsrc_list if tsrc_list is None else tuple(tsrc_list)
-        formatter = _dC.formatter(file, self.statsType)
-        data = formatter.format(tsrc_list)
-        return _dM.dataStats(*data, self.statsType)
+    # def dataStats(self, file: str, tsrc_list: list=None) -> _dM.DataStats:
+    #     tsrc_list = self.tsrc_list if tsrc_list is None else tuple(tsrc_list)
+    #     formatter = _dC.formatter(file, self.statsType)
+    #     data = formatter.format(tsrc_list)
+    #     return _dM.DataStats(*data, self.statsType)
     
-    def dataStats_data(self, data: np.ndarray, tsrc_list: list=None) -> _dM.dataStats:
-        tsrc_list = self.tsrc_list if tsrc_list is None else tuple(tsrc_list)
-        return _dM.dataStats(data, self.statsType)
-
-    # TODO at the moment it's the same as dataStats
-    def corrStats(self, file: str, tsrc_list: list=None) -> _dM.dataStats:
-        tsrc_list = self.tsrc_list if tsrc_list is None else tuple(tsrc_list)
-        formatter = _dC.formatter(file, self.statsType)
-        data = formatter.format(tsrc_list)
-        return _dM.corrStats(*data, self.statsType)
-    
-    def set_folder_gauge(self, path):
-        self.folder_gauge = path
-    
-    def set_folder_data(self, path):
-        self.folder_data = path
+    # def dataStats_data(self, data: np.ndarray, tsrc_list: list=None) -> _dM.DataStats:
+    #     tsrc_list = self.tsrc_list if tsrc_list is None else tuple(tsrc_list)
+    #     return _dM.DataStats(data, self.statsType)
 
 
-    def add_global(self, name: str, value) -> None:
-        """Add global variables to the analysis."""
-        if not name in self.globals: # FIXME: is that correct? Or should be over the keys?
-            self.globals[name] = value
-        else:
-            raise _AlreadyDefined(
-                message = f"Global variable '{name}' already defined with value: {value}"
-            )
+    # def add_global(self, name: str, value) -> None:
+    #     """Add global variables to the analysis."""
+    #     if not name in self.globals: # FIXME: is that correct? Or should be over the keys?
+    #         self.globals[name] = value
+    #     else:
+    #         raise _AlreadyDefined(
+    #             message = f"Global variable '{name}' already defined with value: {value}"
+    #         )
        
-    def _update_database(self):
-        self.get = database() # clear the 'get' operation
+    # def _update_database(self):
+    #     self.get = database() # clear the 'get' operation
 
-        for an_func in tuple(self.database.__dict__.values()):
-            func = an_func(self)
-            if not hasattr(self.get, func.__name__):
-                setattr(self.get, func.__name__, func)
-            else:
-                raise _AlreadyDefined(
-                    message = f"Database function '{func}' already defined."
-                )
+    #     for an_func in tuple(self.database.__dict__.values()):
+    #         func = an_func(self)
+    #         if not hasattr(self.get, func.__name__):
+    #             setattr(self.get, func.__name__, func)
+    #         else:
+    #             raise _AlreadyDefined(
+    #                 message = f"Database function '{func}' already defined."
+    #             )
+    
+    def add_database_analysis(self, database):
+        self.get = database
+        self.get.set_stats(self.statsType, self.tsrc_list)
+
+    def add_database_gauge(self, database):
+        self.db_gauge = database
+    
+    def add_database_data(self, database):
+        self.db_data = database
+
+
 
     # TODO: use a common method to add_* and see if I can use try..except instead of if-else+raise
-    def add_func(self, an_func):
-        """Add functions that retrieves the desired data."""
-        if not hasattr(self.database, an_func.__name__):
-            setattr(self.database, an_func.__name__, an_func)
-        else:
-            raise _AlreadyDefined(
-                message = f"Database raw function '{an_func}' already defined."
-            )
+    # def add_func(self, an_func):
+    #     """Add functions that retrieves the desired data."""
+    #     if not hasattr(self.database, an_func.__name__):
+    #         setattr(self.database, an_func.__name__, an_func)
+    #     else:
+    #         raise _AlreadyDefined(
+    #             message = f"Database raw function '{an_func}' already defined."
+    #         )
 
-        func = an_func(self)
-        if not hasattr(self.get, func.__name__):
-            setattr(self.get, func.__name__, func)
-        else:
-            raise _AlreadyDefined(
-                message = f"Database function '{func}' already defined."
-            )
+    #     func = an_func(self)
+    #     if not hasattr(self.get, func.__name__):
+    #         setattr(self.get, func.__name__, func)
+    #     else:
+    #         raise _AlreadyDefined(
+    #             message = f"Database function '{func}' already defined."
+    #         )
     
-    def add_file(self, file):
-        """Add functions that retrieves the desired data."""
-        if not hasattr(self.files, file.__name__):
-            setattr(self.files, file.__name__, file)
-        else:
-            raise _AlreadyDefined(
-                message = f"Database function '{file}' already defined."
-            )
+    # def add_file(self, file):
+    #     """Add functions that retrieves the desired data."""
+    #     if not hasattr(self.files, file.__name__):
+    #         setattr(self.files, file.__name__, file)
+    #     else:
+    #         raise _AlreadyDefined(
+    #             message = f"Database function '{file}' already defined."
+    #         )
     
-    def add_obj(self, an_obj):
-        """Add object (class)."""
-        if not hasattr(self.objdatabase, an_obj.__name__):
-            setattr(self.objdatabase, an_obj.__name__, an_obj)
-        else:
-            raise _AlreadyDefined(
-                message = f"Class/object '{an_obj}' already defined."
-            )
+    # def add_obj(self, an_obj):
+    #     """Add object (class)."""
+    #     if not hasattr(self.objdatabase, an_obj.__name__):
+    #         setattr(self.objdatabase, an_obj.__name__, an_obj)
+    #     else:
+    #         raise _AlreadyDefined(
+    #             message = f"Class/object '{an_obj}' already defined."
+    #         )
 
-        obj = an_obj(self)
-        if not hasattr(self.obj, obj.__name__):
-            setattr(self.obj, obj.__name__, obj)
-        else:
-            raise _AlreadyDefined(
-                message = f"Class/obj '{obj}' already defined."
-            )
+    #     obj = an_obj(self)
+    #     if not hasattr(self.obj, obj.__name__):
+    #         setattr(self.obj, obj.__name__, obj)
+    #     else:
+    #         raise _AlreadyDefined(
+    #             message = f"Class/obj '{obj}' already defined."
+    #         )
   
-    def print_globals(self):
-        _print_title("GLOBAL VARIABLES")
-        for key, value in self.globals.items():
-            print(f"# {key} =", value)
-        _print_bar()
+    # def print_globals(self):
+    #     _print_title("GLOBAL VARIABLES")
+    #     for key, value in self.globals.items():
+    #         print(f"# {key} =", value)
+    #     _print_bar()
 
   
 
--- analysis/_database.py
+++ analysis/_database.py
@@ -1,9 +1,7 @@
 import numpy as np
-from LatticeABC import dataManager as _dM
-from LatticeABC.dataManager import dataContainer as _dC
-import os
-from os.path import dirname, abspath 
 from functools import wraps
+from LatticeABC import data as _dM
+from ..data import container as _dC
 
 
 
@@ -13,111 +11,196 @@ class Getter:
         attr = vars(self).keys()
         return attr
 
+# This classes are intended for the following firs steps of the analysis
+# - define the naming convention based on the raw data output exluding 
+#   any info on {source, config, extension}
+# - add function for the raw data with {source, config, extension} through
+#   the help of a Database object that does the job for us
+# - finally build the database for the data. This will be the final key object,
+#   i.e. the one that knows how retrieve/format/combine data adn will be
+#   directly used in the analysis. Steps here are:
+#   - define files that stores the raw data as 'gauge' data all together,
+#     i.e. a single file for all sources/config
+#   - define files that stores data from fits ('stats' type)
+#   - define files/functions that define new quantity built from the gauge data
+#     and fits. NB: it is not necessary to store every data, for some it's 
+#     enough to register the functions that combines the data
 
 
-class DatabaseFiles:
-    
+class Database:
+    """Class for naming convention of raw files, without config ID and
+    extension"""
+
     def __init__(self):
-        self.register = {}
+        self._register = {}
     
-    def add_file(self, file_func, path):
-        """Add file as an attribute to the data base."""
+    def add_file(self, file_func):
+        """Add function that returns the file name as an attribute to 
+        the data base."""
+
         key = file_func.__name__
-        if not key in self.register.keys():
-
-            def file_func_path(file_func):
-                @wraps(file_func)
-                def wrapper(*args, **kwargs):
-                    return path + file_func(*args, **kwargs)
-                return wrapper
-
-            self.register[key] = file_func
-            setattr(self, file_func.__name__, file_func_path(file_func))
+        if not key in self._register.keys():
+            self._register[key] = file_func
+            setattr(self, file_func.__name__, file_func)
         else:
             raise ValueError(
-                message = f"Database file '{file_func}' already defined."
+                message = f"Database file {file_func.__name__} already defined."
             )
+        return file_func
 
-class DatabaseData:
+class DatabaseWrapped(Database):
+
+    def __init__(self, databaseName, wrapper):
+        self._wrapper = wrapper
+
+        register = {}
+        if databaseName is not None:
+            for name, func in databaseName._register.items():
+                register[name] = self._wrapper(func)
+                setattr(self, name, self._wrapper(func))
+        self._register = register
+
+    def add_file(self, file_func):
+        """Add function that returns the file name as an attribute to 
+        the data base."""
+
+        key = file_func.__name__
+        if not key in self._register.keys():
+            self._register[key] = self._wrapper(file_func)
+            setattr(self, file_func.__name__, self._wrapper(file_func))
+        else:
+            raise ValueError(
+                message = f"Database file {file_func.__name__} already defined."
+            )
+        return self._wrapper(file_func)
     
-    def __init__(self, databaseFiles, *, tsrc_list=None, statsType=None):
-        self.databaseFiles = databaseFiles
-        self.tsrc_list = tsrc_list
-        self.statsType = statsType
-        self.register = {}
+    def add_data(self, group, complex):
+        """Add function that returns the file name as an attribute to 
+        the data base."""
+
+        out = self._wrapper(group, complex)
+        self._register[out] = out
+        setattr(self, out.__name__, out)
+        return out
+
+
+
+# class DatabaseData:
+    
+#     def __init__(self, database, statsType, tsrc_list):
+#         self.database = database
+#         self.tsrc_list = tsrc_list
+#         self.statsType = statsType
+#         self._register = {}
         
         
-        for key in databaseFiles.register.keys():
-            file_func = getattr(databaseFiles, key)
-            self._add_data(file_func)
+#         for key in database._register.keys():
+#             file_func = getattr(database, key)(self.statsType, self.tsrc_list)
+#             self._add_data(file_func)
             
         
-    def _add_data(self, file_func):
-        """Add data as an attribute to the data base."""
-        if not file_func.__name__ in self.register.keys():
-            def data_func(file_func):
-                def wrapper(tsrc_list=self.tsrc_list, statsType=self.statsType):
-                    @wraps(file_func)
-                    def _data_func(*args, **kwargs):
-                        formatter = _dC.formatter(file_func(*args, **kwargs), statsType)
-                        data = formatter.format(tsrc_list)
-                        return _dM.dataStats(*data, statsType)
-                    return _data_func
-                return wrapper
+#     def _add_data(self, file_func):
+#         """Add data as an attribute to the data base."""
+#         if not file_func.__name__ in self._register.keys():
+#             def data_func(file_func):
+#                 def wrapper(tsrc_list=self.tsrc_list, statsType=self.statsType):
+#                     @wraps(file_func)
+#                     def _data_func(*args, **kwargs):
+#                         formatter = _dC.Formatter(file_func(*args, **kwargs), statsType)
+#                         data = formatter.format(statsType, tsrc_list)
+#                         return _dM.DataStats(*data, statsType)
+#                     return _data_func
+#                 return wrapper
         
-            self.register[file_func.__name__] = data_func(file_func)
-            setattr(self, file_func.__name__, data_func(file_func)())
+#             self._register[file_func.__name__] = data_func(file_func)
+#             setattr(self, file_func.__name__, data_func(file_func))
+#         else:
+#             raise ValueError(
+#                 message = f"Database function '{file_func}' already defined."
+#             )
+        
+
+class DatabaseAnalysis:
+    
+    def __init__(self):
+        self.tsrc_list = None
+        self.statsType = None
+        self._register = {}
+
+
+    def set_stats(self, statsType, tsrc_list):
+        self.statsType = statsType
+        self.tsrc_list = tsrc_list
+        self._apply_stats()
+    
+    def _apply_stats(self):
+        for func in self._register.values():
+            setattr(self, func.__name__, func(self.statsType, self.tsrc_list))
+            
+    def add_func_stats(self, file_func):
+        if not file_func.__name__ in self._register.keys():
+            self._register[file_func.__name__] = file_func
+            # setattr(self, file_func.__name__, file_func)
         else:
             raise ValueError(
                 message = f"Database function '{file_func}' already defined."
             )
-        
-
-class DatabaseFunc:
     
-    def __init__(self, databaseData):
-        self.databaseData = databaseData
-        self.register = {}
-        self.get = Getter()
-
-    @staticmethod
-    def assign_keywords(_func=None, **kwargs):
-        """Decorator to map arguments of a generic function to the desired data."""
-        def decorator(func):
-            @wraps(func)
-            def wrapper(**kwargs_wrapper):
-                new_kwargs = {}
-                for key in kwargs.keys():
-                    if key in kwargs_wrapper.keys():
-                        new_kwargs[key] = kwargs_wrapper[key]
-                    else:
-                        new_kwargs[key] = kwargs[key]
-                return func(**new_kwargs)
-            return wrapper
-        if _func is None:
-            return decorator
-        else:
-            return decorator(_func)
-    
-    def _register(self, func):
-        if not func.__name__ in self.register.keys():
-            setattr(self.get, func.__name__, func)
-            self.register[func.__name__] = func
+    def add_func(self, file_func):
+        if not file_func.__name__ in self._register.keys():
+            #self._register_func[file_func.__name__] = file_func
+            setattr(self, file_func.__name__, file_func)
         else:
             raise ValueError(
-                message = f"Database function '{func}' already defined."
+                message = f"Database function '{file_func}' already defined."
             )
 
-    def add_func(self, _func=None, **kwargs):
-        """Add function as an attribute to the data base."""
-        if _func is not None:
-            self._register(_func)
-        else:
-            def wrap_decorator(func):
-                new_func = self.assign_keywords(_func, **kwargs)(func)
-                self._register(new_func)
-                return new_func #self._keyword_decorator(_func, **kwargs)(func)
-            return wrap_decorator    
+
+# class DatabaseFunc:
+    
+#     def __init__(self, databaseData):
+#         self.databaseData = databaseData
+#         self.register = {}
+#         self.get = Getter()
+
+#     @staticmethod
+#     def assign_keywords(_func=None, **kwargs):
+#         """Decorator to map arguments of a generic function to the desired data."""
+#         def decorator(func):
+#             @wraps(func)
+#             def wrapper(**kwargs_wrapper):
+#                 new_kwargs = {}
+#                 for key in kwargs.keys():
+#                     if key in kwargs_wrapper.keys():
+#                         new_kwargs[key] = kwargs_wrapper[key]
+#                     else:
+#                         new_kwargs[key] = kwargs[key]
+#                 return func(**new_kwargs)
+#             return wrapper
+#         if _func is None:
+#             return decorator
+#         else:
+#             return decorator(_func)
+    
+#     def _register(self, func):
+#         if not func.__name__ in self.register.keys():
+#             setattr(self.get, func.__name__, func)
+#             self.register[func.__name__] = func
+#         else:
+#             raise ValueError(
+#                 message = f"Database function '{func}' already defined."
+#             )
+
+#     def add_func(self, _func=None, **kwargs):
+#         """Add function as an attribute to the data base."""
+#         if _func is not None:
+#             self._register(_func)
+#         else:
+#             def wrap_decorator(func):
+#                 new_func = self.assign_keywords(_func, **kwargs)(func)
+#                 self._register(new_func)
+#                 return new_func #self._keyword_decorator(_func, **kwargs)(func)
+#             return wrap_decorator    
 
 
 
